19086d8cceaab6af518fe406ae1a3a371619d02a
shafigullinik <shafigullin.ik@gmail.com>
2019-11-16 09:46:14 +0300

    gendiff.py ti the first step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,12 @@
+import argparse
+
+
 def main():
-    print("Hi")
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file', type=str, help='First file name')
+    parser.add_argument('second_file', type=str, help='second file name')
+    args = parser.parse_args()
+    print(args.first_file)
 
 
 if __name__ == "__main__":
